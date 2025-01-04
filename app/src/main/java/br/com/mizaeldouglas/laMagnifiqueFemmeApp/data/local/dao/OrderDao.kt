d917c122998dc9df12d5ec90ab30c9fada0db0de
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2025-01-04 16:29:45 -0300

    feat: refactor DAOs
    Signed-off-by: MizaelDouglasDeMello <mizaeldouglas4@gmail.com>

--- app/src/main/java/br/com/mizaeldouglas/laMagnifiqueFemmeApp/data/local/dao/OrderDao.kt
+++ app/src/main/java/br/com/mizaeldouglas/laMagnifiqueFemmeApp/data/local/dao/OrderDao.kt
@@ -7,20 +7,20 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import br.com.mizaeldouglas.laMagnifiqueFemmeApp.data.local.entity.Order
 
-@Dao // Anotação que define a interface como um DAO
+@Dao
 interface OrderDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE) // Inserção de um pedido, substituindo se houver conflito
-    suspend fun insert(order: Order) // Função suspensa para inserir um pedido
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(order: Order)
 
-    @Query("SELECT * FROM `order` ORDER BY order_date DESC") // Consulta para obter todos os pedidos ordenados pela data do pedido em ordem decrescente
-    fun getAllOrders(): LiveData<List<Order>> // Função para obter todos os pedidos como LiveData
+    @Query("SELECT * FROM `order` ORDER BY order_date DESC")
+    fun getAllOrders(): LiveData<List<Order>>
 
-    @Query("SELECT * FROM `order` ORDER BY order_date DESC LIMIT 1") // Consulta para obter o pedido mais recente
-    fun getLatestOrder(): LiveData<Order> // Função para obter o pedido mais recente como LiveData
+    @Query("SELECT * FROM `order` ORDER BY order_date DESC LIMIT 1")
+    fun getLatestOrder(): LiveData<Order>
 
-    @Query("DELETE FROM `order` WHERE id = :orderId") // Consulta para deletar um pedido pelo ID
-    suspend fun deleteOrderById(orderId: Int) // Função suspensa para deletar um pedido pelo ID
+    @Query("DELETE FROM `order` WHERE id = :orderId")
+    suspend fun deleteOrderById(orderId: Int)
 
-    @Query("DELETE FROM `order`") // Consulta para deletar todos os pedidos
-    suspend fun deleteAllOrders() // Função suspensa para deletar todos os pedidos
+    @Query("DELETE FROM `order`")
+    suspend fun deleteAllOrders()
 }
